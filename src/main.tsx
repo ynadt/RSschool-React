54acd0dad770e812615702fc2cd73a2f41f0fc66
ynadt <2769286@gmail.com>
2024-07-05 22:11:45 +0300

    feat: add ErrorBoundary

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
 import './index.css';
+import App from './App';
+import ErrorBoundary from './ErrorBoundary';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+root.render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>,
 );
