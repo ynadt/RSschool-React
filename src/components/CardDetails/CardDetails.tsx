da09544a2cee28cbe472cbef5aca5fccd8a91df5
ynadt <2769286@gmail.com>
2024-07-22 22:33:44 +0300

    fix: fix loader in CardDetails when changing cards

--- src/components/CardDetails/CardDetails.tsx
+++ src/components/CardDetails/CardDetails.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useGetAnimeDetailsQuery } from '@/redux/services/apiSlice.ts';
 import styles from './CardDetails.module.css';
 import type { Anime, ApiError } from '@/types/types.ts';
@@ -10,9 +10,23 @@ interface ItemDetailsProps {
 }
 
 const CardDetails: React.FC<ItemDetailsProps> = ({ id, onClose }) => {
-  const { data, error, isLoading } = useGetAnimeDetailsQuery(id);
+  const { data, error, isLoading, refetch } = useGetAnimeDetailsQuery(id);
+  const [loading, setLoading] = useState(true);
 
-  if (isLoading || !data) return <Loader isLoading={isLoading} error={error as ApiError} />;
+  useEffect(() => {
+    setLoading(true);
+    refetch().then(() => setLoading(false));
+  }, [id, refetch]);
+
+  useEffect(() => {}, [id, isLoading, data]);
+
+  if (loading || isLoading) {
+    return <Loader isLoading={true} error={error as ApiError} />;
+  }
+
+  if (!data) {
+    return <Loader isLoading={true} error={error as ApiError} />;
+  }
 
   const item: Anime = data.data;
 
