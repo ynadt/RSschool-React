f140372cce0e3020c3dd9964bede1b534f358427
ynadt <2769286@gmail.com>
2024-07-23 12:16:47 +0300

    feat: add tests for new Card.tsx

---
+++ tests/setupTests.tsx
@@ -0,0 +1,23 @@
+import '@testing-library/jest-dom';
+import { render as rtlRender, RenderOptions } from '@testing-library/react';
+import { ReactElement } from 'react';
+import { Provider } from 'react-redux';
+import store from '@/redux/store';
+
+// Mock ResizeObserver
+class ResizeObserver {
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+}
+
+global.ResizeObserver = ResizeObserver;
+
+const customRender = (ui: ReactElement, options?: RenderOptions) =>
+  rtlRender(ui, {
+    wrapper: ({ children }) => <Provider store={store}>{children}</Provider>,
+    ...options,
+  });
+
+export * from '@testing-library/react';
+export { customRender };
--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(() => {
     test: {
       globals: true,
       environment: 'jsdom',
-      setupFiles: './tests/setupTests.ts',
+      setupFiles: './tests/setupTests.tsx',
       include: ['tests/**/*.test.ts', 'tests/**/*.test.tsx'],
       coverage: {
         provider: 'v8',
